043088e8830bd52696cce7b9c7bfa3c889845c62
Petro <gliv@mail.ru>
2016-08-19 21:11:29 +0300

    Initial proj

---
+++ Fanat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+
+    class Fun : Human
+    {
+         
+        public Band[] groups { get; set; }
+        public void Shout()
+        {
+        }
+        
+    }
+}

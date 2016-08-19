2eb5c91ecf95470d3decd6a59b8f0c3d42ad2eec
Petro <gliv@mail.ru>
2016-08-19 21:26:05 +0300

    next

--- Human.cs
+++ Human.cs
@@ -54,13 +54,6 @@ namespace Inheritance
         {
             Console.WriteLine("Human is dieng");
         }
-        public  void askdjl()
-        {
-            Console.WriteLine("Human is dieng");
-        }
-        public  void asdasd()
-        {
-            Console.WriteLine("Human is dieng");
-        }
+      
     }
 }
